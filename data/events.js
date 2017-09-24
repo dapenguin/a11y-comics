20f3d8c26834c3365d7fd2b32317cf1fe64a9d4a
dapenguin <anthony.jeffery@littleblueweb.com>
2017-09-24 20:39:34 +0100

    Added event data

---
+++ data/events.js
@@ -0,0 +1,18 @@
+let id = 0;
+
+module.exports = {
+	"events": [
+		{
+			"id": id++,
+			"name": "Stan Lee Signing",
+			"date": "30-07-2017",
+			"info": "Consectetur adipisicing elit. Officia qui architecto, commodi reiciendis aliquid praesentium culpa repellat enim provident explicabo, dolorem aperiam, labore sint in. Harum ratione nesciunt vel commodi!"
+		},
+		{
+			"id": id++,
+			"name": "Star Wars Panel",
+			"date": "17-08-2017",
+			"info": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia qui architecto, commodi reiciendis aliquid praesentium culpa repellat enim provident explicabo, dolorem aperiam, labore sint in. Harum ratione nesciunt vel commodi!"
+		}
+	]
+};
