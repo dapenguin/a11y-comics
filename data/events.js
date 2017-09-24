4d813080266c65793b8bed737423c8d8dbb0105b
dapenguin <anthony.jeffery@littleblueweb.com>
2017-09-24 21:16:04 +0100

    Events now taken from data file

--- data/events.js
+++ data/events.js
@@ -4,13 +4,13 @@ module.exports = {
 	"events": [
 		{
 			"id": id++,
-			"name": "Stan Lee Signing",
+			"title": "Stan Lee Signing",
 			"date": "30-07-2017",
 			"info": "Consectetur adipisicing elit. Officia qui architecto, commodi reiciendis aliquid praesentium culpa repellat enim provident explicabo, dolorem aperiam, labore sint in. Harum ratione nesciunt vel commodi!"
 		},
 		{
 			"id": id++,
-			"name": "Star Wars Panel",
+			"title": "Star Wars Panel",
 			"date": "17-08-2017",
 			"info": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia qui architecto, commodi reiciendis aliquid praesentium culpa repellat enim provident explicabo, dolorem aperiam, labore sint in. Harum ratione nesciunt vel commodi!"
 		}
